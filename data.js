9413c7cbd2b3f4275902c4cc41634ebe92f0a557
yuldashalijal <123844902+yuldashalijal@users.noreply.github.com>
2023-01-29 11:14:55 -0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,39 @@
+let songs = [
+  {
+    name: "song 1",
+    path: "musics/Song-1.mp3",
+    artist: "artist 1",
+    cover: "images/cover-1.jpeg",
+  },
+  {
+    name: "song 2",
+    path: "musics/Song-2.mp3",
+    artist: "artist 2",
+    cover: "images/cover-2.jpeg",
+  },
+  {
+    name: "song 3",
+    path: "musics/Song-3.mp3",
+    artist: "artist 3",
+    cover: "images/cover-3.jpeg",
+  },
+  {
+    name: "song 4",
+    path: "musics/Song-4.mp3",
+    artist: "artist 4",
+    cover: "images/cover-4.png",
+  },
+  {
+    name: "song 5",
+    path: "musics/Song-5.mp3",
+    artist: "artist 5",
+    cover: "images/cover-5.jpeg",
+  },
+  {
+    name: "song 6",
+    path: "musics/Song-6.mp3",
+    artist: "artist 6",
+    cover: "images/cover-7.png",
+  },
+  
+];
